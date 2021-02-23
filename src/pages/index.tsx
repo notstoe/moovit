698428b8b72450a60d9c01965f88b2a486d51752
notstoe <gstonin95@gmail.com>
2021-02-23 19:00:56 +0000

    initial commit, countdown btn, styles

---
+++ src/pages/index.tsx
@@ -0,0 +1,30 @@
+import { CompletedChallenges } from "../Components/CompletedChallenges";
+import { Countdown } from "../Components/Countdown";
+import { ExperienceBar } from "../Components/ExperienceBar";
+import { Profile } from "../Components/Profile";
+
+import Head from 'next/head';
+
+import styles from '../styles/pages/Home.module.css';
+
+export default function Home() {
+	return (
+	<div className={styles.container}>
+
+		<Head>
+			<title>Home | Moovit </title>
+		</Head>
+
+		<ExperienceBar />
+
+		<section>
+			<div>
+				<Profile />
+				<CompletedChallenges />
+				<Countdown />
+			</div>
+			<div></div>
+		</section>
+	</div>
+	);
+}
