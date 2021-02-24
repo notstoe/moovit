0520a64a34e585c2fcf039657e879cdf0ae09af5
notstoe <gstonin95@gmail.com>
2021-02-24 19:04:58 +0000

    functions, timer over

--- src/Components/Countdown.tsx
+++ src/Components/Countdown.tsx
@@ -1,10 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
 import styles from "../styles/components/Countdown.module.css";
 
 export function Countdown() {
+	const { startNewChallenge } = useContext(ChallengesContext);
+
 	let countdownTimeout: NodeJS.Timeout;
 
-	const [time, setTime] = useState(0.05 * 60);
+	const [time, setTime] = useState(25 * 60);
 	const [isActive, setIsActive] = useState(false);
 	const [hasFinished, setHasFinished] = useState(false);
 
@@ -32,6 +35,7 @@ export function Countdown() {
 		} else if (isActive && time == 0) {
 			setHasFinished(true);
 			setIsActive(false);
+			startNewChallenge();
 		}
 	}, [isActive, time]);
 
